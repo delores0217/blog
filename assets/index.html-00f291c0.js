e2a576e18b797fad601b287b3b7dcf9480a528d0
Yalan Wen <delores0217@YalandeMacBook-Pro.local>
2023-08-13 11:45:39 +0800

    deploy

---
+++ assets/index.html-00f291c0.js
@@ -0,0 +1,31 @@
+import{_ as s}from"./plugin-vue_export-helper-c27b6911.js";import{r as l,o as d,c as o,a as e,b as n,d as t,e as a}from"./app-3258b326.js";const r={},c=e("p",null,"This is a blog home page demo.",-1),v=e("p",null,[n("To use this layout, you should set both "),e("code",null,"layout: BlogHome"),n(" and "),e("code",null,"home: true"),n(" in the page front matter.")],-1),m={href:"https://theme-hope.vuejs.press/guide/blog/home/",target:"_blank",rel:"noopener noreferrer"},u=a(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>projects:
+  - icon: project
+    name: project name
+    desc: project detailed description
+    link: https://your.project.link
+
+  - icon: link
+    name: link name
+    desc: link detailed description
+    link: https://link.address
+
+  - icon: book
+    name: book name
+    desc: Detailed description of the book
+    link: https://link.to.your.book
+
+  - icon: article
+    name: article name
+    desc: Detailed description of the article
+    link: https://link.to.your.article
+
+  - icon: friend
+    name: friend name
+    desc: Detailed description of friend
+    link: https://link.to.your.friend
+
+  - icon: /logo.svg
+    name: custom item
+    desc: Detailed description of this custom item
+    link: https://link.to.your.friend
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1);function b(p,h){const i=l("ExternalLinkIcon");return d(),o("div",null,[c,v,e("p",null,[n("For related configuration docs, please see "),e("a",m,[n("blog homepage"),t(i)]),n(".")]),u])}const f=s(r,[["render",b],["__file","index.html.vue"]]);export{f as default};
